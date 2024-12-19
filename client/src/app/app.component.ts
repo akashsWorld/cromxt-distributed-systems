647f061219881d810afe0f15099049662f5eaab1
Akash Biswas <akashbiswas.271999@gmail.com>
2024-12-19 14:30:15 +0530

    Some update.

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,12 +1,11 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'client';
+  title = 'Cloud Store';
 }
